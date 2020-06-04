7b35d55cc3ff048d0f2bcbcbf5573c8d509d9938
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-04 21:41:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from telegram.ext import Updater
 updater = Updater(token='1285306305:AAHuPNLroWn6JU4ocZegsQQ7pTooyegB1eI', use_context=True)
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
