201bac873cdc5d82ec06cdc7d7cc71d4705af8a0
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-04 23:05:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from telegram.ext import Updater
-updater = Updater(token='1296488130:AAH_Qi3WpsvlHy8Q9A-CEqib1TLjSxIw0kI', use_context=True)
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+# Read more about setting it up
+# https://medium.com/@ljmocic/make-telegram-bot-for-notifying-about-new-rss-feed-items-4cfbcc37f4fd
+
+from datetime import timedelta, datetime
+from dateutil import parser
+from pprint import pprint
+from time import sleep
+import requests
+import feedparser
+
+BOT_TOKEN = '1296488130:AAH_Qi3WpsvlHy8Q9A-CEqib1TLjSxIw0kI'
+CHANNEL_ID = '-1001114081400' # @bot_channel_name
+FEED_URL = '' # https://something.com/feeds/rss.xml
+
+
+def send_message(message):
+    requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHANNEL_ID}&text={message}')
+
+
+def main():
+    rss_feed = feedparser.parse(FEED_URL)
+
+    for entry in rss_feed.entries:
+
+        parsed_date = parser.parse(entry.published)
+        parsed_date = (parsed_date - timedelta(hours=8)).replace(tzinfo=None) # remove timezone offset
+        now_date = datetime.utcnow()
+
+        published_20_minutes_ago = now_date - parsed_date < timedelta(minutes=20)
+        if published_20_minutes_ago:
+            send_message(entry.links[0].href)
+            print(entry.links[0].href)
+
+
+if __name__ == "__main__":
+    while(True):
+        main()
+        sleep(20 * 60)
